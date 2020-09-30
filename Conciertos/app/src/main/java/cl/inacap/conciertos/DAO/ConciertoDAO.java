b2d7b84041b166baa4cd1170865143776c1d1ebf
Pedro alfaro <guerrero.03@outlook.com>
2020-09-30 17:46:37 -0300

    30-09-2020
    listo el ingresar
    listo el mostrar
    solo falta el layout personalizado y la personalizar el android
    pd: presionar dos veces en el edittext de Fecha

--- Conciertos/app/src/main/java/cl/inacap/conciertos/DAO/ConciertoDAO.java
+++ Conciertos/app/src/main/java/cl/inacap/conciertos/DAO/ConciertoDAO.java
@@ -1,4 +1,18 @@
 package cl.inacap.conciertos.DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.inacap.conciertos.DTO.Concieto;
+
 public class ConciertoDAO {
+    private static List<Concieto> Conciertos= new ArrayList<>();
+
+    public void Agregarconcierto(Concieto Con){
+        Conciertos.add(Con);
+    }
+
+    public List<Concieto> mostrarConcierto(){
+        return Conciertos;
+    }
 }
--- Conciertos/app/src/main/java/cl/inacap/conciertos/DTO/Concieto.java
+++ Conciertos/app/src/main/java/cl/inacap/conciertos/DTO/Concieto.java
@@ -1,4 +1,54 @@
 package cl.inacap.conciertos.DTO;
 
 public class Concieto {
+    private String Nombre;
+    private String Musica;
+    private String Fecha;
+    private int calificacion;
+    private int precio;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getMusica() {
+        return Musica;
+    }
+
+    public void setMusica(String musica) {
+        Musica = musica;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return  Fecha +" "+Nombre + " " + precio +" "+ calificacion;
+    }
 }
