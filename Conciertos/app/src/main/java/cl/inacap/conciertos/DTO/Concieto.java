1cdfc6d8a78ecc63d8cc53c721b54741840178a2
Pedro alfaro <guerrero.03@outlook.com>
2020-10-01 19:44:58 -0300

    ultimo avance
    Solo faltaba poner los colores en la clasificacion
    
    posdata : presionar el edittext de fecha 2 veces

--- Conciertos/app/src/main/java/cl/inacap/conciertos/DTO/Concieto.java
+++ Conciertos/app/src/main/java/cl/inacap/conciertos/DTO/Concieto.java
@@ -48,7 +48,24 @@ public class Concieto {
     }
 
     @Override
-    public String toString() {
-        return  Fecha +" "+Nombre + " " + precio +" "+ calificacion;
+    public String toString(){
+        String conclusion = new String();
+        if (calificacion==1){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"esto es una estafa";
+        }else if (calificacion==2){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"apesta";
+        }else if (calificacion==3){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"Aburrido";
+        }else if (calificacion==4){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"Hay mejores";
+        }else if (calificacion==5){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"Bien";
+        }else if (calificacion==6){
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"Increible";
+        }else {
+            conclusion=Fecha+" "+Nombre+" "+precio+" "+"Exelente";
+        }
+        //return Fecha+""+Nombre+""+precio+""+calificacion;
+        return conclusion;
     }
 }
